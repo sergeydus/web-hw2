dc038b488101d45065db5149e1d27fb1a92cf0d8
DESKTOP-GHPUBQT\gogos <gogosergey@gmail.com>
2019-05-05 12:53:22 +0300

    added base change to calculator, fixed some html bugs
    added base change
    added bad input detection
    fixed a couple of bugs in the html

--- calc.js
+++ calc.js
@@ -1,3 +1,7 @@
+var base=10;
+function ConvertBase(number, initial_base, change_base) {
+     return parseInt(number, initial_base).toString(change_base);
+}
 function takeValue(x) {
 	document.getElementById('inputwindow').value += x;
 }
@@ -10,6 +14,31 @@ function calculateResult() {
 	var result = eval(document.getElementById('inputwindow').value);
 	document.getElementById('inputwindow').value = result;
 }
+function Changebase(){
+    let prev=base;
+    base = parseInt(document.getElementById("mySelect").value);
+    let text = $("#inputwindow").val();
+    if($("#Warning").css("display")=="none" && text.length>0){
+        let number = parseInt(text);
+        let res=ConvertBase(number,prev,base);
+        $("#inputwindow").val(res);
+    }
+}
+function CheckInput(text){
+    if(!isNaN(parseInt(+text))||text.length==0){
+        //will return a number if its a number
+        $('#Warning').hide(100);
+        $('#inputwindow').css('background-color', '#999');
+    }
+    else{
+        //returns error otherwise
+        $('#inputwindow').css('background-color', 'red');
+        $('#Warning').show(100);
+    }
+}
+$("#calculator").submit(function(e) {
+    e.preventDefault();
+});
 /*
 (function(){
 
